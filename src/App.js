576e8c99ebb14ffdb709a67a3155ff6388ef4d15
Pubudu Janith <pubudujanith123@gmail.com>
2023-03-12 14:21:53 +0530

    Create Login page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import SharedLayout from "./pages/SharedLayout";
+import Home from "./pages/Home";
+import SubmitStudent from "./pages/SubmitStudent";
+import GetStudent from "./pages/GetStudent";
+import UpdateStudent from "./pages/UpdateStudent";
+import DeleteStudent from "./pages/DeleteStudent";
+import Login from "./pages/Login";
+
 function App() {
   return (
-    <>
-      <h1>Hello React</h1>
-    </>
+      <BrowserRouter>
+          <Routes>
+              <Route path={"/"} element={<Login/>}/>
+              <Route path={"dashboard"} element={<SharedLayout/>}>
+                  <Route index element={<Home/>}/>
+                  <Route path={"submit"} element={<SubmitStudent/>}/>
+                  <Route path={"get"} element={<GetStudent/>}/>
+                  <Route path={"update"} element={<UpdateStudent/>}/>
+                  <Route path={"delete"} element={<DeleteStudent/>}/>
+              </Route>
+          </Routes>
+      </BrowserRouter>
   );
 }
 
---
+++ src/pages/Login.js
@@ -0,0 +1,9 @@
+function Login() {
+    return (
+        <>
+
+        </>
+    );
+}
+
+export default Login;
