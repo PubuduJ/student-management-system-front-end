5bbe73e9466785b63964176df5ccf4a998bd66fd
Pubudu Janith <pubudujanith123@gmail.com>
2023-03-12 15:31:49 +0530

    Complete GetStudent logic

--- src/pages/GetStudent.js
+++ src/pages/GetStudent.js
@@ -1,24 +1,49 @@
 import {useState} from "react";
 import {Link} from "react-router-dom";
 import OutputContainer from "../components/OutputContainer";
+import {GetCall} from "../api/ApiCalls";
 
 function GetStudent() {
     const [output, setOutput] = useState({nic: "", name: "", address: "", contact: ""});
     const [nic, setNic] = useState("");
     const [errMessage, setErrMessage] = useState("");
+    const [responseMessage, setResponseMessage] = useState("");
 
     function handleChange(event) {
         setOutput({nic: "", name: "", address: "", contact: ""});
+        setResponseMessage("");
+        setErrMessage("");
         const newNic = event.target.value;
         setNic(newNic);
     }
 
-    function handleSubmit(event) {
+    async function handleSubmit(event) {
         event.preventDefault();
         if (!/^\d{9}[Vv]$/.test(nic)) {
             setErrMessage("User nic number is empty or invalid");
             document.getElementById("nic").focus();
             return;
+        }
+        try {
+            const response = await GetCall(nic);
+            setResponseMessage("User successfully get from the database");
+            setOutput({
+                nic: response.data.nic,
+                name: response.data.name,
+                address: response.data.address,
+                contact: response.data.contact
+            });
+        }
+        catch (err) {
+            if (err.response) {
+                setResponseMessage(err.response.data.message);
+            }
+            else {
+                setResponseMessage(`Error: ${err.message}`);
+            }
+        }
+        finally {
+            setNic("");
         }
     }
 
@@ -41,6 +66,8 @@ function GetStudent() {
                     address={output.address}
                     contact={output.contact}
                 />
+                <br/>
+                <h4>{responseMessage}</h4>
             </div>
         </div>
     );
